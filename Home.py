7345eec3d89bda5e958b94828f3b5a3df35c0bd1
laminatorsix <miralujutin@gmail.com>
2026-08-13 01:27:25 +0800

    Add disclaimer

--- Home.py
+++ Home.py
@@ -12,6 +12,26 @@ st.set_page_config(
     layout="wide"
 )
 
+with st.expander("⚠️ IMPORTANT NOTICE — Proof of Concept", expanded=True):
+
+    st.warning(
+        """
+        **IMPORTANT NOTICE:** This web application is developed as a
+        proof-of-concept prototype.
+
+        The information provided here is **NOT intended for actual usage**
+        and should not be relied upon for making any decisions, especially
+        those related to financial, legal, or healthcare matters.
+
+        **Furthermore, please be aware that the LLM may generate inaccurate
+        or incorrect information. You assume full responsibility for how
+        you use any generated output.**
+
+        Always consult with qualified professionals for accurate and
+        personalised advice.
+        """
+    )
+
 st.title("🔐 BEACON Access Assistant")
 
 st.write(
